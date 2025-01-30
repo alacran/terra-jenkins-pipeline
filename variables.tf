78994a72cba3c5f7a0544a037d959a6d20bb3f78
alacran <rodrigosilva027@gmail.com>
2025-01-30 17:05:11 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "aws_region" {
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   default     = "ami-04b4f1a9cf54c11d0"
 }
 
 variable "instance_type" {
